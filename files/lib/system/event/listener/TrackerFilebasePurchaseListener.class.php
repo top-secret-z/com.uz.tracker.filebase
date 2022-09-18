2627ec81fc667af97cd2362f839f02261fd65ca3
Sascha Greuel <github@1-2.dev>
2022-09-18 22:45:02 +0200

    5.5.1

--- files/lib/system/event/listener/TrackerFilebasePurchaseListener.class.php
+++ files/lib/system/event/listener/TrackerFilebasePurchaseListener.class.php
@@ -1,59 +1,89 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace filebase\system\event\listener;
+
 use wcf\data\package\PackageCache;
 use wcf\data\user\tracker\log\TrackerLogEditor;
-use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\cache\builder\TrackerCacheBuilder;
+use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\WCF;
 
 /**
  * Listen to Filebase purchase page.
- * 
- * @author		2016-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.tracker.filebase
  */
-class TrackerFilebasePurchaseListener implements IParameterizedEventListener {
-	/**
-	 * tracker and link
-	 */
-	protected $tracker = null;
-	protected $link = '';
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function execute($eventObj, $className, $eventName, array &$parameters) {
-		if (!MODULE_TRACKER) return;
-		
-		// only if user is to be tracked
-		$user = WCF::getUser();
-		if (!$user->userID || !$user->isTracked || WCF::getSession()->getPermission('mod.tracking.noTracking')) return;
-		
-		// only if trackers
-		$trackers = TrackerCacheBuilder::getInstance()->getData();
-		if (!isset($trackers[$user->userID])) return;
-		
-		$this->tracker = $trackers[$user->userID];
-		if (!$this->tracker->wlfilFilePurchase) return;
-		
-		$this->link = $eventObj->file->getLink();
-		$this->store('wcf.uztracker.description.filebase.purchase', 'wcf.uztracker.type.wlfil');
-	}
-	
-	/**
-	 * store log entry
-	 */
-	protected function store ($description, $type, $name = '', $content = '') {
-		$packageID = PackageCache::getInstance()->getPackageID('com.uz.tracker.filebase');
-		TrackerLogEditor::create(array(
-				'description' => $description,
-				'link' => $this->link,
-				'name' => $name,
-				'trackerID' => $this->tracker->trackerID,
-				'type' => $type,
-				'packageID' => $packageID,
-				'content' => $content
-		));
-	}
+class TrackerFilebasePurchaseListener implements IParameterizedEventListener
+{
+    /**
+     * tracker and link
+     */
+    protected $tracker;
+
+    protected $link = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function execute($eventObj, $className, $eventName, array &$parameters)
+    {
+        if (!MODULE_TRACKER) {
+            return;
+        }
+
+        // only if user is to be tracked
+        $user = WCF::getUser();
+        if (!$user->userID || !$user->isTracked || WCF::getSession()->getPermission('mod.tracking.noTracking')) {
+            return;
+        }
+
+        // only if trackers
+        $trackers = TrackerCacheBuilder::getInstance()->getData();
+        if (!isset($trackers[$user->userID])) {
+            return;
+        }
+
+        $this->tracker = $trackers[$user->userID];
+        if (!$this->tracker->wlfilFilePurchase) {
+            return;
+        }
+
+        $this->link = $eventObj->file->getLink();
+        $this->store('wcf.uztracker.description.filebase.purchase', 'wcf.uztracker.type.wlfil');
+    }
+
+    /**
+     * store log entry
+     */
+    protected function store($description, $type, $name = '', $content = '')
+    {
+        $packageID = PackageCache::getInstance()->getPackageID('com.uz.tracker.filebase');
+        TrackerLogEditor::create([
+            'description' => $description,
+            'link' => $this->link,
+            'name' => $name,
+            'trackerID' => $this->tracker->trackerID,
+            'type' => $type,
+            'packageID' => $packageID,
+            'content' => $content,
+        ]);
+    }
 }
--- update_page.sql
+++ update_page.sql
@@ -1,5 +1,5 @@
-INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\FilePage', 'file', 1);
-INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\CategoryFileListPage', 'categoryFileList', 1);
-INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\CategoryFileListFeedPage', 'rssFeedCategoryFile', 1);
-INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\DownloadPage', 'download', 1);
-INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\FileListFeedPage', 'rssFeedFile', 1);
+INSERT INTO    wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\FilePage', 'file', 1);
+INSERT INTO    wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\CategoryFileListPage', 'categoryFileList', 1);
+INSERT INTO    wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\CategoryFileListFeedPage', 'rssFeedCategoryFile', 1);
+INSERT INTO    wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\DownloadPage', 'download', 1);
+INSERT INTO    wcf1_user_tracker_page (class, page, isPublic) VALUES ('filebase\\page\\FileListFeedPage', 'rssFeedFile', 1);
